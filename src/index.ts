1ed338510d16089a684409c10277b942fc1219ac
andygargol2010-afk <andygargol2010@gmail.com>
2026-06-16 19:51:06 -0300

    ambas consignas terminadas

--- src/index.ts
+++ src/index.ts
@@ -21,10 +21,53 @@ DARIO       |       50%
 */
 
 // COMPLETAR: Implementar la función calcularTotal que reciba el id del artista, la cantidad de entradas y un código de descuento (opcional) y devuelva el precio total a pagar en patacones.
+function calcularTotal(id: string, cantidad: number, codigoDescuento?: string): number {
+  let precio: number = 0;
+  if(id === 'sabrina') {
+    precio = 1000 * cantidad;
+  }
+  else if(id === 'kgatlw') {
+    precio = 700 * cantidad;
+  }
+  else if(id === 'lali') {
+    precio = 500 * cantidad;
+  }
+  else if(id === 'magdalena') {
+    precio = 600 * cantidad;
+  }
+  else if(id === 'viagra') {
+    precio = 400 * cantidad;
+  }
+  else if(id === 'dillom') {
+    precio = 350 * cantidad;
+  }
+  else if(id === 'marilina') {
+    precio = 200 * cantidad;
+  }
+  else if(id === 'mugre') {
+    precio = 150 * cantidad;
+  }
+  else {
+    console.log('ID de artista no válido');
+  }
+
+  if(codigoDescuento === 'TIC10') {
+    precio = precio * 0.9;
+  }
+  else if(codigoDescuento === 'TIC20') {
+    precio = precio * 0.8;
+  }
+  else if(codigoDescuento === 'DARIO') {
+    precio = precio * 0.5;
+  }
+
+  return precio;
+}
+
 
 cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
   let cantidadNum: number = Number(cantidad);
-  let precio: number = calcularTotal(id, cantidadNum, codigoDescuento);
+  let precio: number =calcularTotal(id, cantidadNum, codigoDescuento);
   enviarAlFrontend('mostrarPrecio', precio);
 });
 
